77f2f4deba27d3f65ef8b0602ff875d3a36d44c2
ninjavanxuantruong <ninjavanxuantruong@gmail.com>
2026-03-27 16:31:05 +0700

    Add googleSheetLinks.js with sheet URLs

---
+++ exercises/link.js
@@ -0,0 +1,11 @@
+// googleSheetLinks.js
+const SHEET_URL = "https://script.google.com/macros/s/AKfycbxzedXjLuoot-1WVNBegypFaEv4IAPtI_2Vyjb1Ie9wr7B7rMcxt0n2s77E-vwyCjVM9Q/exec";
+const SHEET_BAI_HOC = "https://script.google.com/macros/s/AKfycbwGc5T0Ei1CA3-LnxSv9JxqqnkL5WeyZFTbgEu0vZTq25E8gypYbP-FRJApNa5oJr12tQ/exec";
+
+// Export cho cả browser và Node.js
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { SHEET_URL, SHEET_BAI_HOC };
+} else {
+    window.SHEET_URL = SHEET_URL;
+    window.SHEET_BAI_HOC = SHEET_BAI_HOC;
+}
